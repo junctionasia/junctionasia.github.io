b4a9f3b4b4f28b0d620a8badcf2482d0ae401de8
shibatanaoto <naoto.shibata510@gmail.com>
2016-03-11 17:03:27 +0900

    debug insertgs

--- scripts/insertgs.js
+++ scripts/insertgs.js
@@ -10,15 +10,16 @@ function appendRow(){
 
     token = "ya29.ogKXckmQKBsYM_oXBwoh8S0fCAmTpWhqXrqlE_7IWepleIzOALIfOxsqQx_Hzp_epQ"
 
-    var params = constructAtomXML("foo");
-    spredsheetId = '18zbjMH8OzeTaqWzeLEyEU2_bL7Xg9-dO844kF41SDNU';
-    url = 'https://spreadsheets.google.com/feeds/list/'+spredsheetId+'/default/private/full?alt=json&access_token=' + token;
+    var params = constructAtomXML(email);
+    spreadsheetId = '18zbjMH8OzeTaqWzeLEyEU2_bL7Xg9-dO844kF41SDNU';
+    url = 'https://spreadsheets.google.com/feeds/list/'+spreadsheetId+'/default/private/full?alt=jsonp&access_token=' + token;
 
     var z = new XMLHttpRequest();
     z.open("POST", url, true);
     z.setRequestHeader("Content-type", "application/atom+xml");
     z.setRequestHeader("GData-Version", "3.0");
     z.setRequestHeader("Authorization", 'Bearer '+ token);
+    z.setRequestHeader("Access-Control-Allow-Origin", "*");
     z.onreadystatechange = function() {//Call a function when the state changes.
         if(z.readyState == 4 && z.status == 200) {
             alert(z.responseText);
