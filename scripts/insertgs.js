2cd7cf27b3d0e79fca972367c33b338fff65e008
kkshmz <kyeshimizu@Qgmail.com>
2016-03-16 15:02:20 +0900

    Input name json

--- scripts/insertgs.js
+++ scripts/insertgs.js
@@ -1,8 +1,9 @@
 function send_mail(){
     mail_address = $('input[id=form_email]').val();
-    name = $('input[id=form_email]').val();
+    name = $('input[id=name]').val();
     $('input[id=from_email]').val("");
-    $('input[class=send_button]').val("Thank you!")
+    $('input[id=name]').val("");
+    $('input[class=send_button]').val("Subscribed!")
     url = "http://ec2-52-192-39-102.ap-northeast-1.compute.amazonaws.com/mail?body=" + mail_address + + "&name=" + name;
     $.ajax({
          url: url,
