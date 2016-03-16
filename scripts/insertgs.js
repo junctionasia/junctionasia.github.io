2ce2eb4e803070d3ac7d11eaced5715ad201149b
kkshmz <kyeshimizu@Qgmail.com>
2016-03-16 12:05:00 +0900

    Cancel in country name

--- scripts/insertgs.js
+++ scripts/insertgs.js
@@ -1,7 +1,6 @@
 function send_mail(){
     mail_address = $('input[id=form_email]').val();
     name = $('input[id=form_email]').val();
-    country = $('input[id=name]').val();
     $('input[id=from_email]').val("");
     $('input[type=button]').val("Thank You");
     url = "http://ec2-52-192-39-102.ap-northeast-1.compute.amazonaws.com/mail?body=" + mail_address + "&name=" + name + "&country=" + country;
@@ -15,11 +14,3 @@ function send_mail(){
          }
     });
 }
-
-
-$.getJSON('../country.json', function(json){
-    countries = json.country;
-    for (var i = countries.length - 1; i >= 0; i--) {
-        $('select#country').append($('<option>').html(countries[i]).val(countries[i]));
-    }
-});
